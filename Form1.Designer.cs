649d74e1667490bba1b6046c11a33f729b3bb495
HyperXewl <stefanbuzianczuk@gmail.com>
2023-03-30 16:59:16 +1000

    YT Downloader

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace YTDownloader
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.RadioButton rdoVideo;
+        private System.Windows.Forms.RadioButton rdoMp3;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label labelProgress;
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.rdoVideo = new System.Windows.Forms.RadioButton();
+            this.rdoMp3 = new System.Windows.Forms.RadioButton();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(92, 42);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(450, 29);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "";
+            // 
+            // rdoVideo
+            // 
+            this.rdoVideo.AutoSize = true;
+            this.rdoVideo.Checked = true;
+            this.rdoVideo.Location = new System.Drawing.Point(92, 114);
+            this.rdoVideo.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.rdoVideo.Name = "rdoVideo";
+            this.rdoVideo.Size = new System.Drawing.Size(88, 29);
+            this.rdoVideo.TabIndex = 2;
+            this.rdoVideo.TabStop = true;
+            this.rdoVideo.Text = "Video";
+            this.rdoVideo.UseVisualStyleBackColor = true;
+            // 
+            // rdoMp3
+            // 
+            this.rdoMp3.AutoSize = true;
+            this.rdoMp3.Location = new System.Drawing.Point(198, 114);
+            this.rdoMp3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.rdoMp3.Name = "rdoMp3";
+            this.rdoMp3.Size = new System.Drawing.Size(78, 29);
+            this.rdoMp3.TabIndex = 3;
+            this.rdoMp3.Text = "MP3";
+            this.rdoMp3.UseVisualStyleBackColor = true;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(28, 183);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(517, 42);
+            this.btnDownload.TabIndex = 4;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProgress.Location = new System.Drawing.Point(22, 247);
+            this.labelProgress.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(528, 74);
+            this.labelProgress.TabIndex = 6;
+            this.labelProgress.Text = "---";
+            this.labelProgress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 318);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.rdoMp3);
+            this.Controls.Add(this.rdoVideo);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YouTube Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using System;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+
+namespace YTDownloader
+{
+    public partial class Form1 : Form
+    {
+        private bool isDownloading = false;
+        private string youtubeDlPath = "";
+        //private bool enableDebug = false;
+        private string downloaderExe = "youtube-dl-external.exe";
+        //private int totalBytes = 0;
+        //private int downloadedBytes = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            youtubeDlPath = System.IO.Path.Combine(Application.StartupPath, downloaderExe);
+            if (!System.IO.File.Exists(youtubeDlPath))
+            {
+                MessageBox.Show(downloaderExe + " not found. Please put it in the same directory as this program and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            txtUrl.TextChanged += TxtUrl_TextChanged;
+        }
+
+        private void TxtUrl_TextChanged(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            string pattern = @"^(https?://)?(www\.)?(youtube\.com/watch\?v=)([a-zA-Z0-9_-]+).*";
+            Match match = Regex.Match(url, pattern);
+            if (match.Success)
+            {
+                string newUrl = match.Groups[3].Value + match.Groups[4].Value;
+                txtUrl.Text = newUrl;
+                txtUrl.SelectionStart = txtUrl.Text.Length;
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (isDownloading)
+            {
+                return;
+            }
+
+            isDownloading = true;
+            btnDownload.Enabled = false;
+            txtUrl.Enabled = false;
+            rdoMp3.Enabled = false;
+            rdoVideo.Enabled = false;
+            btnDownload.Text = "Downloading...";
+            //progressBar1.Visible = true;
+
+            string url = txtUrl.Text;
+            string youtubeDlCommand = "";
+
+            if (rdoVideo.Checked)
+            {
+                youtubeDlCommand = $"\"{youtubeDlPath}\" -o \"%USERPROFILE%\\Desktop\\%(title)s.%(ext)s\" \"{url}\"";
+            }
+            else if (rdoMp3.Checked)
+            {
+                youtubeDlCommand = $"\"{youtubeDlPath}\" -x --audio-format mp3 -o \"%USERPROFILE%\\Desktop\\%(title)s.%(ext)s\" \"{url}\"";
+            }
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+            worker.WorkerReportsProgress = true;
+            worker.RunWorkerAsync(youtubeDlCommand);
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string youtubeDlCommand = e.Argument as string;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"/C \"{youtubeDlCommand}\"";
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            Console.WriteLine(startInfo.FileName + " " + startInfo.Arguments); // log command to console
+
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += Process_OutputDataReceived;
+            process.ErrorDataReceived += Process_ErrorDataReceived;
+
+            process.Start();
+
+            process.EnableRaisingEvents = true;
+            process.OutputDataReceived += new DataReceivedEventHandler(Process_OutputDataReceived);
+
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+
+            process.WaitForExit();
+        }
+
+        private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(e.Data))
+            {
+                Console.WriteLine("Error: " + e.Data);
+            }
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                if (e.Data.Contains("[download]"))
+                {
+                    int index = e.Data.IndexOf('%');
+                    if (index >= 0)
+                    {
+                        string percentStr = e.Data.Substring(index - 4, 4);
+                        if (double.TryParse(percentStr, out double percent))
+                        {
+                            if (sender != null) // add null check
+                            {
+                                BeginInvoke(new Action(() => {
+                                    labelProgress.Text = percentStr + " %";
+                                }));
+                            }
+                            Console.WriteLine(percentStr);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(e.Data);
+                }
+            }
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            UpdateProgressLabel(e.ProgressPercentage);
+        }
+
+        private void UpdateProgressLabel(int percent)
+        {
+            string progressText = percent.ToString() + "%";
+            Console.WriteLine(progressText);
+            labelProgress.Text = progressText;
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            isDownloading = false;
+            btnDownload.Enabled = true;
+            txtUrl.Enabled = true;
+            rdoMp3.Enabled = true;
+            rdoVideo.Enabled = true;
+            btnDownload.Text = "Download";
+            //progressBar1.Visible = false;
+        }
+    }
+}
