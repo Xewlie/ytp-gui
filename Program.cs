8e967ef537eff8eaf94ee031af7dbdc43f32991f
HyperXewl <stefanbuzianczuk@gmail.com>
2023-04-10 07:17:29 +1000

    7.0 upgrade

--- Program.cs
+++ Program.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+namespace YTDownloader;
 
-namespace YTDownloader
+static class Program
 {
-    internal static class Program
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
 }
